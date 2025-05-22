9b863f915376fca545154ddde4eaf77493747e03
Nihir Kumar <n4nihir@gmail.com>
2025-05-22 08:25:04 +0530

    App and Prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 
 import dill
+import pickle
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
@@ -52,3 +53,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
